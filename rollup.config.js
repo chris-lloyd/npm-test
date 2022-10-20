661f81085bfbd6e3ed445f34c3f33fb88f57a5ca
Christopher LIM CHIN <christopher_lim_chin@hotmail.com>
2022-10-20 14:31:13 +0400

    add new package

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,9 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import postcss from "rollup-plugin-postcss";
+import babel from '@rollup/plugin-babel';
+import external from 'rollup-plugin-peer-deps-external';
+import del from 'rollup-plugin-delete';
 
 // const packageJson = require("./package.json");
 import packageJson from './package.json' assert {type: 'json'};
@@ -28,6 +31,11 @@ export default {
     typescript({ useTsconfigDeclarationDir: true }),
     postcss({
         extensions: ['.css']
-    })
+    }),
+    babel({
+      exclude: 'node_modules/**',
+      babelHelpers: 'bundled' 
+  }),
+    del({ targets: ['lib/*'] }),
   ]
 };
